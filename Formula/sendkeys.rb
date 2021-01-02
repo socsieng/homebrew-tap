c2cf2e67d390fe89648887093b1d9fd62c778ea5
runner <runner@Mac-1609545759579.local>
2021-01-02 00:16:48 +0000

    chore: update sendkeys to v1.1.0

--- Formula/sendkeys.rb
+++ Formula/sendkeys.rb
@@ -1,8 +1,8 @@
 class Sendkeys < Formula
   desc "Command line tool for automating keystrokes and mouse events"
   homepage "https://github.com/socsieng/sendkeys"
-  url "https://github.com/socsieng/sendkeys.git", :tag => "v1.0.0", :revision => "096a714ed398f444b1739f28b25e9e3c8c5ae377"
-  version "1.0.0"
+  url "https://github.com/socsieng/sendkeys.git", :tag => "v1.1.0", :revision => "9b6228f68a822fb2533f570580906bf2b6460dcd"
+  version "1.1.0"
   license "Apache-2.0"
 
   depends_on :xcode => ["12.0", :build]
@@ -21,8 +21,8 @@ class Sendkeys < Formula
   end
 
   bottle do
-    root_url "https://github.com/socsieng/sendkeys/releases/download/v1.0.0"
+    root_url "https://github.com/socsieng/sendkeys/releases/download/v1.1.0"
     cellar :any_skip_relocation
-    sha256 "bf95290c24cb3f94a8b4d070abdbc817b93db39590b437b9c1409d1162f449b9" => :catalina
+    sha256 "0d2ab4de2af607315f20ff9ec78f224f07e1d2df2ec3f9ecfa7ce35984aa6c41" => :catalina
   end
 end
