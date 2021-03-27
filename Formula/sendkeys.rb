6b0545a30a0ed329469d483117b91734fac255db
runner <runner@Mac-1616811671094.local>
2021-03-27 02:35:15 +0000

    chore: update sendkeys to v2.3.7

--- Formula/sendkeys.rb
+++ Formula/sendkeys.rb
@@ -1,8 +1,8 @@
 class Sendkeys < Formula
   desc "Command line tool for automating keystrokes and mouse events"
   homepage "https://github.com/socsieng/sendkeys"
-  url "https://github.com/socsieng/sendkeys.git", :tag => "v2.3.0", :revision => "007525c6eea3698ded30301f090c8950d1ac8136"
-  version "2.3.0"
+  url "https://github.com/socsieng/sendkeys.git", :tag => "v2.3.7", :revision => "591e4ec7b4db72b73cb25b270403864f349038e3"
+  version "2.3.7"
   license "Apache-2.0"
 
   depends_on :xcode => ["12.0", :build]
@@ -21,10 +21,10 @@ class Sendkeys < Formula
   end
 
   bottle do
-    root_url "https://github.com/socsieng/sendkeys/releases/download/v2.3.0"
+    root_url ""
     cellar :any_skip_relocation
-    sha256 "312b6f5e786f623e9859283884b86ae49decfc5025c5b705c6486d918115d2b5" => :catalina
-    # sha256 "312b6f5e786f623e9859283884b86ae49decfc5025c5b705c6486d918115d2b5" => :big_sur
-    # sha256 "312b6f5e786f623e9859283884b86ae49decfc5025c5b705c6486d918115d2b5" => :arm64_big_sur
+    sha256 "e610ed4d4b5d5b48dd9bb98613102330727694fa1a5cc3eb8c052849842b0a05" => :catalina
+    sha256 "abf3eaa0e7cd84fe8d4640755ce6c53f953fcd6deadfb5bd31dbebc894db8f5f" => :big_sur
+    sha256 "16595ae37c7e466370affe4f7f080f71b797152591fe98997becfa023e41732b" => :arm64_big_sur
   end
 end
