54483f118563b384c3acdd08696a3b2a9a786cc8
runner <runner@Mac-1624119573361.local>
2021-06-19 17:27:14 +0000

    chore: update sendkeys to v2.5.2

--- Formula/sendkeys.rb
+++ Formula/sendkeys.rb
@@ -1,15 +1,15 @@
 class Sendkeys < Formula
   desc "Command-line tool for automating keystrokes and mouse events"
   homepage "https://github.com/socsieng/sendkeys"
-  url "https://github.com/socsieng/sendkeys.git", tag: "v2.4.0", revision: "69f8c7718797c5dbb3323f5562624ddeecf7f300"
-  version "2.4.0"
+  url "https://github.com/socsieng/sendkeys.git", tag: "v2.5.2", revision: "a459fc8f6ed33c7bbc42a1fbb552721a641e5092"
+  version "2.5.2"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/socsieng/sendkeys/releases/download/v2.4.0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "8b44362905ced4a8988a6292ad1fdb4749c901d90d6953b2becd57013ccc88b4"
-    sha256 cellar: :any_skip_relocation, catalina:      "2678d90af53756fbbc072c73e3df6a2cfcc0ee7ad5da3a261bc1d21f3408ca27"
-    sha256 cellar: :any_skip_relocation, big_sur:       "111b1b8e772a0d91650beb1762d0092ae27efec89255debb7125efb3d1d74e4e"
+    root_url "https://github.com/socsieng/sendkeys/releases/download/v2.5.2"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "1466e14f353894e78a72eaaa12dd9850271d98561b5dd94e9cf7df5a6205198e"
+    sha256 cellar: :any_skip_relocation, catalina:      "70dea18e1b29720c31f7323578655fb57393daad1694c39e58a87c6e3849b711"
+    sha256 cellar: :any_skip_relocation, big_sur:       "4d94e8681f993aea99ba33ca80fc7f97f95cdc54c02effea9cbc7b9f65b800b7"
   end
 
   pour_bottle? do
