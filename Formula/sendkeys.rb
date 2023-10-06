1e35acad73507804ac8bf944d7338686f12abe69
Soc Sieng <socsieng@users.noreply.github.com>
2023-10-06 13:12:45 +1100

    chore: remove support for macOS catalina

--- Formula/sendkeys.rb
+++ Formula/sendkeys.rb
@@ -8,7 +8,6 @@ class Sendkeys < Formula
   bottle do
     root_url "https://github.com/socsieng/sendkeys/releases/download/v2.9.1"
     sha256 cellar: :any_skip_relocation, arm64_big_sur: "dd965a3a4f7f1ee63f2edaf31d6b7242649590531216ed7a1fa7dbaa1d14791c"
-    sha256 cellar: :any_skip_relocation, catalina:      "46d9d2118eb27509cb93ac8b53d1a1b5dca5eb6e166252aa247a485d12f1a4af"
     sha256 cellar: :any_skip_relocation, big_sur:       "2286c10389c809e0a07747e3d94c13d12b751825b3dde0349ad5e5452bb66503"
   end
 
