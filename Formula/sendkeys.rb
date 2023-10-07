abcb778d5c3a2fc2b14d4f2ec7f01f279d407a5c
runner <runner@Mac-1696678442664.local>
2023-10-07 12:51:12 +0000

    chore: update sendkeys to v4.0.3

--- Formula/sendkeys.rb
+++ Formula/sendkeys.rb
@@ -1,14 +1,14 @@
 class Sendkeys < Formula
   desc "Command-line tool for automating keystrokes and mouse events"
   homepage "https://github.com/socsieng/sendkeys"
-  url "https://github.com/socsieng/sendkeys.git", tag: "v4.0.0", revision: "f46e5da792d4dfa00fbaca5992cbd01145a340cd"
-  version "4.0.0"
+  url "https://github.com/socsieng/sendkeys.git", tag: "v4.0.3", revision: "46d744f84b50e5521c1486cf987297f2902740a6"
+  version "4.0.3"
   license "Apache-2.0"
 
   bottle do
-    root_url "https://github.com/socsieng/sendkeys/releases/download/v4.0.0"
-    sha256 cellar: :any_skip_relocation, arm64_big_sur: "f4478f87a35f7719fa597b3db80f445d1ded969da3c04bf31f6a11371dfc31a3"
-    sha256 cellar: :any_skip_relocation, big_sur:       "ef942284fd25aed0a63a19db348a8454032d6e20ef5fc8fa20d6751e49f9cd43"
+    root_url "https://github.com/socsieng/sendkeys/releases/download/v4.0.3"
+    sha256 cellar: :any_skip_relocation, arm64_big_sur: "bbb111d8089268e31c68ef826d80c5c99e7d04f1980d39ec3dfd67961be9cde3"
+    sha256 cellar: :any_skip_relocation, big_sur:       "c7158129014aada4edad1864b1f76791d3a1f9396c36d43d4331061def27b2bb"
   end
 
   pour_bottle? do
